7b60eb52e28dfdff2ac020e5b3824f11fe431dfe
cursedhunter <chad@elitepg.net>
2022-12-02 12:33:39 +0000

    just testing traceroute from leaf3A to dns 8.8.8.8

--- batfish/policies/test_base.py
+++ batfish/policies/test_base.py
@@ -365,11 +365,11 @@ def test_reachability_to_server_dc1_pod1_2(bf):
     bf.asserts.current_assertion = 'Assert reachability to server-dc1-pod1-2 from dc1-pod1-leaf3A'
 
     headers = HeaderConstraints(dstIps='server-dc1-pod1-2',srcIps='8.8.8.8', applications='DNS')
-    result = bf.q.reachability(pathConstraints=PathConstraints(startLocation = 'dc1-pod1-leaf1a[Loopback0]'), headers=headers, actions='SUCCESS').answer().frame()
+    result = bf.q.reachability(pathConstraints=PathConstraints(startLocation = 'dc1-pod1-leaf3a[Loopback0]'), headers=headers, actions='SUCCESS').answer().frame()
 
     write_to_csv_output(result, "Reachability-Server-DC1-POD1-2")
     record_results(bf, status=STATUS_PASS,
-                   message='Reachability from dc1-pod1-leaf1a to server-2')
+                   message='Reachability from dc1-pod1-leaf3a to server-2')
 
 # def test_reachability_to_server_dc1_pod2_2(bf):
 #    os.environ['bf_policy_name'] = "Base configuration Hygiene Policies"
